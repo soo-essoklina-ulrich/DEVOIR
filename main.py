b7e7831b3d2ca8c9097fce28f732747ad13a2fba
soo-essoklina-ulrich <127897124+soo-essoklina-ulrich@users.noreply.github.com>
2023-06-24 14:32:43 +0000

    connect and create DB with ORM 2

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-from fastapi import FastAPI
-from models import Vehicule, Proprio, vehicules, Categories
+from fastapi import FastAPI, Depends
+from models import Vehicule, Proprio, vehicules as Pyvehicules, Categories
 from typing import List
 from db import Session
 
 
 app = FastAPI()
 
- 
+
 @app.get("/category/{category}")
 async def category(category: str) -> List[Vehicule]:
     return []
 
+
 @app.get("/owner/{owner}")
 async def owner(owner: str) -> List[Vehicule]:
-    return [vehicle for vehicle in vehicules if vehicle.owner == owner]
+    return [vehicle for vehicle in Pyvehicules if vehicle.owner == owner]
+
 
 @app.get("/bills/{owner}")
 async def bills(owner: str) -> List[Proprio]:
     bills = []
-    for vehicle in vehicules:
+    for vehicle in Pyvehicules:
         if vehicle.owner == owner:
             total = vehicle.cotation + vehicle.facture
             bill = Proprio(
@@ -31,9 +33,20 @@ async def bills(owner: str) -> List[Proprio]:
             bills.append(bill)
     return bills
 
+
 def get_db():
     db = Session()
     try:
         yield db
     finally:
         db.close()
+
+
+@app.post("/add-new", response_model=Vehicule)
+async def add_new(vh: Vehicule, db: Session = Depends(get_db)):
+    vhs = Pyvehicules(id=vh.id, immatricule=vh.immatricule, owner=vh.owner, cotation=vh.cotation, facture=vh.facture)
+    db.add(vhs)
+    db.commit()
+    db.refresh(vhs)
+
+    return Pyvehicules(**vh.dict())
